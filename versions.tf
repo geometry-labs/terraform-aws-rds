0d79ff2b17dde7bd1db5dc796008085a4167c4eb
robcxyz <rob@geometrylabs.io>
2021-05-18 00:42:23 -0600

    add vault provider to versions

--- versions.tf
+++ versions.tf
@@ -2,7 +2,11 @@ terraform {
   required_version = ">= 0.12.26"
 
   required_providers {
-    aws = {
+   vault = {
+      source = "hashicorp/vault"
+      version = ">= 2.19"
+    }
+   aws = {
       source  = "hashicorp/aws"
       version = ">= 2.49"
     }
